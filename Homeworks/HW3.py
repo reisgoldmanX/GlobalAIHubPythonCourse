50a032086095239dbd700aa82f922b19c985c804
reisX <46068107+reisgoldmanX@users.noreply.github.com>
2021-03-11 13:13:49 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,13 @@
-#Explain your work
+quiz = [100, 100, 100]
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def cal_average(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t
+
+    avg = sum_num / len(num)
+    return avg
+
+
+print("The average is", cal_average(quiz))
